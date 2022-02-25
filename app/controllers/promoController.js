9cd7a98d437e700325c006ee9a6ed44f34c08ad3
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-02-25 01:28:02 +0100

    creating a separate module to create DB client and connect it

--- app/controllers/promoController.js
+++ app/controllers/promoController.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 // we connect to the database
-const pg = require('pg');
-const client = new pg.Client(process.env.PG_URL);
-client.connect();
+const client = require('../dbClient');
 
 const promoController = {
 
@@ -33,31 +31,38 @@ const promoController = {
     // Getting the promo which matches the target id
     // Using paramerterized query to avoid string concatenating parameters directly (tehrefore preventing SQL injections)
     client.query('SELECT * FROM "promo" WHERE "id" = $1', [targetId], (error, data) => {
-        if (error) {
-            // If the promo is not found, we return an 500-status error
-            res.status(500).send(error);
+      if (error) {
+        // If the promo is not found, we return an 500-status error
+        res.status(500).send(error);
+      } else {
+        // If there is a match among promos, we save it in a variable
+        // Since data.rows is an array, we save the first element of the array 
+        const promo = data.rows[0];
+
+        //if (promo === undefined)
+        if (!promo) {
+          // we didn't find the promo => 404
+          next();
         } else {
-            // If there is a match among promos, we save it in a variable
-            // Since data.rows is an array, we save the first element of the array 
-            const promo = data.rows[0];
-            // Then we send another query to the database to get all students who belong to this promo
-            client.query('SELECT * FROM "student" WHERE "promo_id" = $1', [targetId], (error, data) => {
-                if (error) {
-                    // If no match is found, we return a 500-status error
-                    res.status(500).send(error);
-                } else {
-                    // If we find students in the DB, we save the returned array in a variable called 'studentsOfPromo'
-                    const studentsOfPromo = data.rows;
-                    // we pass both the promo and the students to the ejs template 
-                    res.render('promoDetails', {
-                        promo,
-                        studentsOfPromo
-                    });
-                }
-            });
-        };
+          // Then we send another query to the database to get all students who belong to this promo
+          client.query('SELECT * FROM "student" WHERE "promo_id" = $1', [targetId], (error, data) => {
+            if (error) {
+              // If no match is found, we return a 500-status error
+              res.status(500).send(error);
+            } else {
+              // If we find students in the DB, we save the returned array in a variable called 'studentsOfPromo'
+              const studentsOfPromo = data.rows;
+              // we pass both the promo and the students to the ejs template 
+              res.render('promoDetails', {
+                promo,
+                studentsOfPromo
+              });
+            }
+          });
+        }
+      };
     });
-},
+  },
 }
 
 module.exports = promoController;
---
+++ app/dbClient.js
@@ -0,0 +1,9 @@
+// creating a PG client, connecting it and exporting it
+
+const pg = require('pg');
+
+const client = new pg.Client(process.env.PG_URL);
+
+client.connect();
+
+module.exports = client;
